6e57d38b9d52843d0a99ed97d0842c0a602ac0dc
Scibuild <30519309+Scibuild@users.noreply.github.com>
2020-06-04 19:36:30 +1000

    testing simple state

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
 
+
+const Center = ({children}) => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }} >
+            {children}
+        </View>)
+}
 
 export default function() {
+    const [state, setState] = useState(false);
     
+
     return (
-        <Text>
-            Welcome to the app.
-        </Text>
+        <Center>
+            <Text style={{paddingBottom: 30}}>
+                Welcome to the app.
+            </Text>
+            
+            {state ? <Text>Not here</Text> : <Text>Here!</Text>}
+
+            <Button onPress={() => setState(s => !s)} title="Press me!" />
+        </Center>
     )
 }
