aec29c5500103317f6921811b81881ef6625f905
Scibuild <30519309+Scibuild@users.noreply.github.com>
2021-04-10 13:24:03 +1000

    theme?

--- src/navigation/Route.tsx
+++ src/navigation/Route.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import MainTabs from "./MainTabs";
 import { preinit } from "../lib/init";
 
 // run before everything
 preinit();
 
+// const AppTheme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors
+//     1
+//   }
+// }
+
 export const MainRoute: React.FC<{}> = () => {
   return (
     <NavigationContainer>
-      {/* <MenuProvider> */}
       <MainTabs />
-      {/* </MenuProvider> */}
     </NavigationContainer>
   );
 };
